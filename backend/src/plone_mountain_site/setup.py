45d7c756b2008a7abdaceab0d75c545127f59826
Fosten <fosten@gmail.com>
2024-08-08 23:35:11 -0400

    Add collective.honeypot

--- backend/src/plone_mountain_site/setup.py
+++ backend/src/plone_mountain_site/setup.py
@@ -52,9 +52,11 @@ setup(
         "collective.volto.formsupport[hcaptcha]",
         "collective.volto.formsupport[recaptcha]",
         "collective.volto.formsupport[norobots]",
+        "collective.volto.formsupport[honeypot]",
         "plone.formwidget.hcaptcha",
         "plone.formwidget.recaptcha",
         "collective.z3cform.norobots",
+        "collective.honeypot",
     ],
     extras_require={
         "test": [
