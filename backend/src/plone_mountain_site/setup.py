8715b9a03c424a540d9f57d7f5de0d52f8d3ca54
Fosten <fosten@gmail.com>
2024-09-30 13:31:25 -0400

    Add volto-banner and eea.banner

--- backend/src/plone_mountain_site/setup.py
+++ backend/src/plone_mountain_site/setup.py
@@ -50,6 +50,7 @@ setup(
         "plone.api",
         "collective.volto.formsupport[honeypot]",
         "collective.honeypot",
+        "eea.banner",
     ],
     extras_require={
         "test": [
