041d122cb4f1e449d876402abf225e5bdd9ca354
Fosten <fosten@gmail.com>
2024-10-25 16:32:11 -0400

    Re-add upgraded cookieplone volto-mountain-theme

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,9 +1,9 @@
 const addons = [
   'volto-mountain-site',
   '@Fosten/volto-farmOS',
-  '@Fosten/volto-mountain-theme'
+  'volto-mountain-theme'
 ];
-const theme = '@Fosten/volto-mountain-theme';
+const theme = 'volto-mountain-theme';
 
 module.exports = {
   addons,
