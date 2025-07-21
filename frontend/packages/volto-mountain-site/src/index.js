a22842a2b462bce004cbaa8f0ea0c0e71f6cb3df
Fosten <fosten@gmail.com>
2025-07-21 08:20:38 -0400

    Bump Volto from 19.0.0-alpha.2 to 19.0.0-alpha.3

--- frontend/packages/volto-mountain-site/src/index.js
+++ frontend/packages/volto-mountain-site/src/index.js
@@ -1,9 +1,6 @@
 const applyConfig = (config) => {
   config.settings = {
     ...config.settings,
-    isMultilingual: false,
-    supportedLanguages: ['en'],
-    defaultLanguage: 'en-us',
     matomoSiteId: '2',
     matomoUrlBase: 'https://stats.lillymountain.com/',
     showPloneLogin: false,
